7c23655d1d29b1b81000905bcef7565ab6d52459
Dan Jaworsky <jiwicky@gmail.com>
2015-03-24 23:49:42 -0400

    farts

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '59de83102c446f3b85cc50c8c89c34ae');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0a32f08a59250c52a82b99f2d38bface');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
