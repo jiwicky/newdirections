72a3426473ac8329b602d9d7e6faa2d203e5fcea
Dan Jaworsky <jiwicky@gmail.com>
2015-03-28 21:10:39 -0400

    fix this css

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '41ba1146463a03878365242ca98f4d28');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fdf75ba39ad18f4968a6bd25dc3e751d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
